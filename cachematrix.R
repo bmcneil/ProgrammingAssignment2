3bc41abd3df7e0c4e6e823ae43415e86ec00310b
Becky McNeil <bbmcneil@gmail.com>
2015-12-27 15:22:52 -0500

    Add code for Coursera R Programming - Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera training R Programming (JHSPH)
+## Assignment 2
 
-## Write a short comment describing this function
+## These functions calculate the matrix inverse and store it
+## for future recall. If the inverse already exists, the
+## cached version is returned.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## get input matrix dims to create NULL version of matrix inverse w/correct dims
+	xdim <- dim(x)
+	xrows <- xdim[1]
+	xcols <- xdim[2]
+## create NULL/NA version of matrix inverse w/correct dims
+	v <- matrix(nrow = xrows, ncol = xcols)
+	set <- function(y) {
+		x <<- y
+		v <<- matrix(nrow = xrows, ncol = xcols)
+	}
+	get <- function() x
+	setinvs <- function(invs) v <<- invs
+	getinvs <- function() v
+	list(set = set,
+		get = get,
+		setinvs = setinvs,
+		getinvs = getinvs)
 }
 
-
-## Write a short comment describing this function
+## This function takes arguments including the list returned by makeCacheMatrix
+## and computes the matrix inverse. It returns either the cached inverse (if it
+## was previously computed) or the newly computed inverse. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	v <- x$getinvs()
+	if(all(!is.na(v))) {
+		message("getting cached data")
+		return(v)
+	}
+	data <- x$get()
+	v <- solve(data)
+	x$setinvs(v)
+	v
 }
